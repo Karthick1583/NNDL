a9fe5271b7f26e3507cdb56bd524f0c00d9ac84d
Saravanak Karthick <sara.karth158@gmail.com>
2024-07-18 11:50:44 +0530

    updated

--- perceptron.py
+++ perceptron.py
@@ -1,20 +1,31 @@
+def summ(w, x1, x2, b):
+    z_list = []
+    for i in range(len(x1)):
+        z = w[0] * x1[i] + w[1] * x2[i] + b
+        z_list.append(z)
+    return z_list
 
-def andd(x1,x2,y,w,b,n,epoch):
-    for i in range(epoch) :
+def sigmoid(z):
+    if z >= 0:
+        y1 = 1
+    else:
+        y1 = 0
+    return y1
+
+def updateweight(y, y1, w, n, x1, x2, b, i):
+    err = y[i] - y1
+    w[0] = w[0] + n * err * x1[i]
+    w[1] = w[1] + n * err * x2[i]
+    b = b + n * err
+    return w, b
+
+def andd(x1, x2, y, w, b, n, epoch):
+    for i in range(epoch):
         for j in range(len(x1)):
-
-                z = w[0]*x1[j] + w[1]*x2[j] + b
-                if z >= 0:
-                    y1 = 1
-                else:
-                    y1 = 0
-                err = y[j]-y1
-                if (err) != 0:
-                     w[0] = w[0] + n*(err)*x1[j]
-                     w[1] = w[1] + n*(err)*x2[j]
-                     b = b + n*(err)
-                print(w)
-                
+            z = w[0] * x1[j] + w[1] * x2[j] + b
+            y1 = sigmoid(z)
+            w, b = updateweight(y, y1, w, n, x1, x2, b, j)
+            print(f'Epoch {i+1}, Sample {j+1}: Weights: {w}, Bias: {b}')
     return x1, x2, y, w, b
 
 x1 = list(map(int, input("Enter 1st variable values: ").split()))
